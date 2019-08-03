bd9b8bd6cf09eeda67d97c509a77c80ab8a39ea8
Sushil <bajracharya.sushil01@gmail.com>
2019-08-03 23:15:44 +0545

    Updates

---
+++ precache-manifest.2167dc3f3d4b483c334593ee59ebef3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c52733147644bc39b14cac3c4976f9",
+    "url": "/PokeDex/index.html"
+  },
+  {
+    "revision": "b55c2073a1c6e0295155",
+    "url": "/PokeDex/static/css/2.d8464a33.chunk.css"
+  },
+  {
+    "revision": "bca8470b6942cf200862",
+    "url": "/PokeDex/static/css/main.3c086361.chunk.css"
+  },
+  {
+    "revision": "b55c2073a1c6e0295155",
+    "url": "/PokeDex/static/js/2.a07b968b.chunk.js"
+  },
+  {
+    "revision": "bca8470b6942cf200862",
+    "url": "/PokeDex/static/js/main.381e65c1.chunk.js"
+  },
+  {
+    "revision": "4959d1521e70ff083e9a",
+    "url": "/PokeDex/static/js/runtime~main.b0bf2daa.js"
+  },
+  {
+    "revision": "f6887db4c28f640d6b6aaaedb911358b",
+    "url": "/PokeDex/static/media/pokeWallpaper.f6887db4.jpg"
+  },
+  {
+    "revision": "49255cb281084c9c58a3e8c86e743a16",
+    "url": "/PokeDex/static/media/pokemon.49255cb2.png"
+  }
+]);
