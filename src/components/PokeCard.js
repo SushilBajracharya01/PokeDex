754e81c05d1284c7503d870057d5520206a59c72
SushilBajracharya <bajracharya.sushil01@gmail.com>
2021-07-11 22:23:35 +0545

    update

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,102 +1,91 @@
-import React, { Component } from 'react';
-import Pokemon from '../pokemon';
-import PokeType from './PokeType'
+import React, { useEffect, useState } from "react";
+import Pokemon from "../pokemon";
+import PokeType from "./PokeType";
 // import PokeDesc from './PokeDesc';
 import Speech from "speak-tts";
 
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
-class PokeCard extends Component {
+import PokeballSvg from "../pokeball.svg";
 
+const PokeCard = ({ url }) => {
+  const [pokemon, setPokemon] = useState({});
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            Pokemons: {},
-        }
-    }
+  useEffect(() => {
+    const fetchPokemons = async (url) => {
+      const res = await fetch(url);
+      const data = await res.json();
+      const pokemon = new Pokemon(data);
+      setPokemon(pokemon);
+    };
+    fetchPokemons(url);
+  }, [url]);
 
-    async fetchPokemons(url) {
-        const res = await fetch(url);
-        const data = await res.json();
-        const pokemon = new Pokemon(data);
-        window.scrollTo(-10, -10);
-        return (
-            this.setState({
-                Pokemons: pokemon,
-            })
-        )
-    }
+  const PokeSpeak = (data) => {
+    const speech = new Speech();
 
+    speech
+      .init()
+      .then((data) => {
+        // The "data" object contains the list of available voices and the voice synthesis params
+        console.log("Speech is ready, voices are available", data);
+      })
+      .catch((e) => {
+        console.error("An error occured while initializing : ", e);
+      });
 
-    componentWillMount() {
-        this.fetchPokemons(this.props.url);
-    }
+    const text = `${data.name}. i.d${data.id}.. a ${
+      data.types
+    } Type Pokemon, its moves are ${data.moves.map((move) => move + ".")} `;
+    speech.cancel();
+    speech
+      .speak({
+        text: text,
+      })
+      .then(() => {
+        console.log("Success !");
+      })
+      .catch((e) => {
+        console.error("An error occurred :", e);
+      });
+  };
 
-    componentWillReceiveProps(newProps) {
-        this.fetchPokemons(newProps.url);
-    }
+  return (
+    <>
+      <div
+        className="col-lg-2 col-md-3  col-4 p-0 m-3 PokeCard"
+        onClick={() => PokeSpeak(pokemon)}
+      >
+        <div className="spriteContainer mt-md-5 mb-md-5">
+          <LazyLoadImage
+            alt=""
+            src={pokemon.sprite}
+            effect="blur"
+            className="pokeSprite"
+          />
+        </div>
+        <div className="nameContainer">
+          <h5 className="pokeName capitalize p-0 mt-2">{pokemon.name}</h5>
 
-    PokeSpeak = (data) => {
-        const speech = new Speech();
-
-        speech.init().then((data) => {
-            // The "data" object contains the list of available voices and the voice synthesis params
-            console.log("Speech is ready, voices are available", data)
-        }).catch(e => {
-            console.error("An error occured while initializing : ", e)
-        })
-
-        const text = `${data.name}. i.d${data.id}.. a ${data.types} Type Pokemon, its moves are ${data.moves.map(move => move + '.')} `;
-        speech.cancel();
-        speech.speak({
-            text: text,
-        }).then(() => {
-            console.log("Success !")
-        }).catch(e => {
-            console.error("An error occurred :", e)
-        })
-    }
-
-    render() {
-        const { Pokemons } = this.state;
-        return (
-            <>
-                <div className="col-lg-2 col-md-3  col-4 p-0 m-3 PokeCard" onClick={() => this.PokeSpeak(Pokemons)}>
-
-                    <div className="spriteContainer mt-md-5 mb-md-5">
-                        <LazyLoadImage
-                            alt=""
-                            src={Pokemons.sprite}
-                            effect="blur"
-                            className="pokeSprite" />
-                    </div>
-                    <div className="nameContainer">
-                        <h5 className="pokeName capitalize p-0 mt-2">{Pokemons.name}</h5>
-
-                        {(Pokemons.types ?
-                            Pokemons.types.map((type, id) => (
-                                <PokeType key={id++} type={type} />))
-                            :
-                            null)}
-                    </div>
-
-                </div>
-                {/* <div className="col-10">
+          {pokemon.types
+            ? pokemon.types.map((type, id) => (
+                <PokeType key={id++} type={type} />
+              ))
+            : null}
+        </div>
+      </div>
+      {/* <div className="col-10">
                 <PokeDesc 
-                    id={Pokemons.id}
-                    name={Pokemons.name}
-                    sprite={Pokemons.sprite}
-                    types={Pokemons.types}
-                    moves={Pokemons.moves}
+                    id={pokemon.id}
+                    name={pokemon.name}
+                    sprite={pokemon.sprite}
+                    types={pokemon.types}
+                    moves={pokemon.moves}
                 />                
             </div> */}
-            </>
-
-        );
-    }
-}
-
+    </>
+  );
+};
 
 export default PokeCard;
--- src/components/PokeDex.js
+++ src/components/PokeDex.js
@@ -1,76 +1,95 @@
-import React, { Component } from 'react';
-import PokeCard from './PokeCard';
-import PokeTitle from '.././pokemon.png';
+import React, { useState, useEffect, useCallback } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
+import PokeCard from "./PokeCard";
+import PokeTitle from ".././pokemon.png";
 
-class PokeDex extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            pokemons: {}
-        }
-        this.fetchPokemon.bind();
-    }
+//assets
+import PokeballSvg from "../pokeball.svg";
 
-    async fetchPokemon (url) {
-        const res = await fetch(url);
-        const pokemons = await res.json();
-        return (
-            this.setState({
-                pokemons:pokemons,
-                isLoading: false
-            })
-        )
-    }
+const PokeDex = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pokemons, setPokemons] = useState({});
+  const [pokemonResults, setPokemonResults] = useState([]);
 
-    async componentDidMount() {
-        this.fetchPokemon("https://pokeapi.co/api/v2/pokemon");
-    }
+  const fetchPokemon = useCallback(
+    async (url) => {
+      const res = await fetch(url);
+      const pokemons = await res.json();
+      console.log(pokemons);
+      setPokemons(pokemons);
+      setPokemonResults([...pokemonResults, ...pokemons.results]);
+      setIsLoading(false);
+    },
+    [pokemonResults]
+  );
 
-    render() {
-        const { pokemons, isLoading } = this.state;
-        return (
-            <div className="PokeDex">
-                <div className="PokeContainer">
-                    <div className="text-center text-light">
-                        <button className="bg-transparent border-0" href="" onClick={()=>this.fetchPokemon("https://pokeapi.co/api/v2/pokemon")}>
-                            <img src={PokeTitle} alt="pokemon" className="pokeTitle" />
-                        </button>
-                        <div className="container">
-                            <div className="row d-flex justify-content-center">
+  useEffect(() => {
+    const fetchInitialPokemon = async () => {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const pokemons = await res.json();
+      setPokemons(pokemons);
+      setPokemonResults([...pokemons.results]);
+      setIsLoading(false);
+    };
 
-                                {(isLoading) ?
-                                    <div>
-                                        <p className="lead">Loading...</p>
-                                    </div>
-                                    :
-                                    // <PokeCard url={pokemons.results[0].url} />
-                                    
-                                    pokemons.results.map((pokemon, i) => (
-                                        <PokeCard key={i++} url={pokemon.url} />
-                                    ))
-                                }
+    fetchInitialPokemon();
+  }, []);
 
-                            </div>
-                        </div>
-                        <div className="BtnContainer">
-                            <button className={`btn btn-primary m-2 ${pokemons.previous?'active ':'disabled'}`} onClick={()=>{
-                                this.fetchPokemon(pokemons.previous)
-                            }}>
-                                Previous
-                            </button>
-                            <button className={`btn btn-primary m-2 ${pokemons.next?'active ':'disabled'}`} onClick={()=>{
-                                this.fetchPokemon(pokemons.next)
-                            }}>
-                                Next
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+  return (
+    <div className="PokeDex">
+      <div className="PokeContainer">
+        <div className="text-center text-light">
+          <button
+            className="bg-transparent border-0"
+            href=""
+            onClick={() => fetchPokemon("https://pokeapi.co/api/v2/pokemon")}
+          >
+            <img src={PokeTitle} alt="pokemon" className="pokeTitle" />
+          </button>
+          <div className="container">
+            {isLoading ? (
+              <div className="loadingDiv">
+                <img
+                  className="pokeballSpinner"
+                  src={PokeballSvg}
+                  alt="pokeball loading spiner"
+                />
+              </div>
+            ) : (
+              <InfiniteScroll
+                dataLength={pokemonResults?.length} //This is important field to render the next data
+                next={() => {
+                  console.log("NEXT CALLED");
+                  fetchPokemon(pokemons.next);
+                }}
+                hasMore={pokemons.next}
+                loader={
+                  <div className="loadingDiv">
+                    <img
+                      className="pokeballSpinner"
+                      src={PokeballSvg}
+                      alt="pokeball loading spiner"
+                    />
+                  </div>
+                }
+                className="dexContainer row d-flex justify-content-center"
+                endMessage={
+                  <p style={{ textAlign: "center" }}>
+                    <b>Yay! You have seen it all</b>
+                  </p>
+                }
+              >
+                {pokemonResults?.map((pokemon) => (
+                  <PokeCard key={pokemon.url} url={pokemon.url} />
+                ))}
+              </InfiniteScroll>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PokeDex;
